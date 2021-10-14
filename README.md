d035ab0060f4b725c5560c25e2d471ad34552881
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-14 11:59:29 -0300

    feat: README and test finishing

---
+++ README.md
@@ -0,0 +1,19 @@
+# Testing (T/J)est
+
+This is just an APP for studying Jest Testing on React Native.
+
+### How to run it
+
+If you are curious about it, just clone this repo on your machine, then - inside the project folder - open the terminal and run:
+
+```Shell
+yarn && yarn test
+```
+
+And that's it.
+
+If you also want to run the app to see it on Expo, run:
+
+```Shell
+yarn start
+```
--- src/__tests__/HomeScreen.test.js
+++ src/__tests__/HomeScreen.test.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { Alert } from "react-native";
 import { fireEvent, render } from "@testing-library/react-native";
-import {} from "jest";
 
 import HomeScreen from "../views/HomeScreen";
 describe("Home", () => {
+  // Getting the function
   jest.spyOn(Alert, "alert");
-  const page = render(<HomeScreen />);
+
+  it("should return True", () => {
+    // Pointing the components to be tested
+    const page = render(<HomeScreen />);
+    const trueButton = page.getByTestId("true_button");
+
+    // Creating the event for the component (pressing it)
+    fireEvent.press(trueButton);
+
+    // What is expected to be returned from the called function
+    expect(Alert.alert).toHaveBeenCalledWith("True");
+  });
+
+  it("should return False", () => {
+    const page = render(<HomeScreen />);
+    const falseButton = page.getByTestId("false_button");
+
+    fireEvent.press(falseButton);
+
+    expect(Alert.alert).toHaveBeenCalledWith("False");
+  });
+
   it("should return True and False", () => {
+    const page = render(<HomeScreen />);
     const trueButton = page.getByTestId("true_button");
     const falseButton = page.getByTestId("false_button");
 
